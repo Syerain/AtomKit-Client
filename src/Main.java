82815a63a5e5fb514193f69f3a9366b8197d3e5b
Syerain <1066505758@qq.com>
2024-09-17 16:31:05 +0800

    by sye_rain

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javafx.stage.Stage;
 import top.syewiki.atk.cli.*;
 //import top.syewiki.atk.cli.gui.*;
 import top.syewiki.atk.cli.stdgui.*;
@@ -5,7 +6,24 @@ import top.syewiki.atk.cli.stdgui.*;
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
-        ClassStdFrameBuilder fb = new ClassStdFrameBuilder();
-        fb.drawUI();
+
+        //ClassStdFrameBuilder fb = new ClassStdFrameBuilder();
+        //fb.drawUI();
+
+        ClassStdTabbedPaneBuilder tp = new ClassStdTabbedPaneBuilder();
+        tp.drawPane();
+
+        //thrdFX thrdfx = new thrdFX();
+        //thrdfx.run();
+
+
     }
 }
+
+/*class thrdFX extends Thread{
+    public void run() {
+        ClassStdFrameBuilderFX fx = new ClassStdFrameBuilderFX();
+        Stage stage = new Stage();
+        fx.start(stage);
+    }
+}*/
